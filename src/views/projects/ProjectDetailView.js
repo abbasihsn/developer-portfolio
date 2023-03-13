6258c83407ce1d0258b34160daaaa0470aecdbc1
Hsn abs <abbasihsn@Hsns-MacBook-Air.local>
2023-03-12 20:21:49 -0400

    project detail view section has app project style now

--- src/views/projects/ProjectDetailView.js
+++ src/views/projects/ProjectDetailView.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import classes from "./ProjectDetailView.module.css"
+import classesApp from "./ProjectDetailViewApp.module.css"
 const ProjectDetailView = ({project, onBack}) =>{
     const [selectedIdx, setSelectedIdx] = useState(0)
-    return <div className={classes.project_detail__container}>
-        <div className={classes.project_detail__text__container}>
+    return <div className={project.type==="app"?classesApp.project_detail__container__app:classes.project_detail__container}>
+        <div className={ project.type==="app"?classesApp.project_detail__text__container__app:classes.project_detail__text__container}>
             <h1>{project.title}</h1>
             <div className={classes.below__title}>
                 <p>{project.tools}</p>
@@ -16,12 +17,12 @@ const ProjectDetailView = ({project, onBack}) =>{
             <p>{project.description}</p>     
         </div>
         
-        <div className={classes.project_detail__container__image}>
-            <div className={classes.small__images__column}>
-                {project.src.map((item,idx)=><img onClick={()=>setSelectedIdx(idx)} alt={project.id} src={project.src[idx]} className={selectedIdx===idx? classes.active :""}/>)}
+        <div className={project.type==="app"?classesApp.project_detail__container__image__app:classes.project_detail__container__image}>
+            <div className={project.type==="app"?classesApp.small__images__column__app:classes.small__images__column}>
+                {project.src.map((item,idx)=><img onClick={()=>setSelectedIdx(idx)} alt={project.id} src={require("./images/" + project.src[idx])} className={selectedIdx===idx? classes.active :""}/>)}
             </div>            
-            <div className={classes.zoomed__image}>
-            <img alt={project.id} src={project.src[selectedIdx]} />
+            <div className={project.type==="app"?classesApp.zoomed__image__app:classes.zoomed__image}>
+                <img alt={project.id} src={require("./images/" + project.src[selectedIdx])} />
             </div>
         </div>
         
--- src/views/projects/ProjectView.js
+++ src/views/projects/ProjectView.js
@@ -1,9 +1,4 @@
 import classes from "./ProjectView.module.css"
-import project1 from "../../assets/project1.png"
-import project2 from "../../assets/project2.jpg"
-import project3 from "../../assets/project3.jpg"
-import jwtProject from "../../assets/jwt_project.jpeg"
-import javaRestProject from "../../assets/java_rest_project.png"
 import ProjectItem from "../../components/projects/ProjectItem"
 import { useState } from "react"
 import ProjectDetailView from "./ProjectDetailView"
@@ -13,11 +8,11 @@ const ProjectView = () => {
 
     const [projectToShow, setProjectToShow] = useState(-1) // -1=> show all prjects
     const projects = [
-        { title: "Developer Portfolio", description: "Website with pure HTMAL, CS and JS without any package! Website with pure HTMAL, CS and JS without any package!", src: [project1, project2,project1,project1], tools: "ReactJS | CSS | HTML", id: 0, type:"web" , demoLink:"http://hasanabbasi.ca", codeLink:"https://github.com/abbasihsn/developer-portfolio"},
-        { title: "Iranian Stock Analysis", description: "It helps you to review the stock market status.", src: [project2, project2, project2,project2], tools: "Flutter | NodeJS | MongoDB", id: 1, type:"app"},
-        { title: "Persian Time Tracker", description: "An application using flutter", src: [project3, project3, project3, project3], tools: "Flutter | NodeJS | MongoDB", id: 2 , type:"app"},
-        { title: "NodeJS JWT authentication", description: "A simple example for JWT token based authentication", src: [jwtProject, jwtProject, jwtProject], tools: "NodeJS | MongoDB", id: 3 , type:"web", codeLink:"https://github.com/abbasihsn/jwt-nodejs-example"},
-        { title: "Java Rest API", description: "A simple example for Java CRUD operation and connection to DB,", src: [javaRestProject, javaRestProject, javaRestProject, javaRestProject,javaRestProject], tools: "Java | PostgreSQL", id: 4 , type:"web", codeLink:"https://github.com/abbasihsn/javaRestExample"},
+        { title: "Developer Portfolio", description: "Website with pure HTMAL, CS and JS without any package! Website with pure HTMAL, CS and JS without any package!", src: ["devPortfolio/project1.png"], tools: "ReactJS | CSS | HTML", id: 0, type:"web" , demoLink:"http://hasanabbasi.ca", codeLink:"https://github.com/abbasihsn/developer-portfolio"},
+        { title: "Iranian Stock Analysis", description: "It helps you to review the stock market status.", src: ["stockMarket/project2.jpg"], tools: "Flutter | NodeJS | MongoDB", id: 1, type:"app"},
+        { title: "Persian Time Tracker", description: "An application using flutter", src: ["RoutineMe/routineMe_1.webp", "RoutineMe/routineMe_2.webp", "RoutineMe/routineMe_4.webp", "RoutineMe/routineMe_3.webp"], tools: "Flutter | NodeJS | MongoDB", id: 2 , type:"app"},
+        { title: "NodeJS JWT authentication", description: "A simple example for JWT token based authentication", src: ["nodeJwt/jwt_project.jpeg"], tools: "NodeJS | MongoDB", id: 3 , type:"web", codeLink:"https://github.com/abbasihsn/jwt-nodejs-example"},
+        { title: "Java Rest API", description: "A simple example for Java CRUD operation and connection to DB,", src: ["javaRest/java_rest_project.png"], tools: "Java | PostgreSQL", id: 4 , type:"web", codeLink:"https://github.com/abbasihsn/javaRestExample"},
     ]
 
     const onClickHandler = (id)=>{
@@ -27,9 +22,8 @@ const ProjectView = () => {
     const onBackHandler = (id)=>{
         setProjectToShow(-1)
     }
-
     let widget = <div className={classes.project_view__container}>
-        {projects.map(item => <ProjectItem onClick={onClickHandler} title={item.title} description={item.description} src={item.src[0]} id={item.id} tools={item.tools} type={item.type} key={item.id} demo={item.demoLink} code={item.codeLink}/>)}
+        {projects.map(item => <ProjectItem onClick={onClickHandler} title={item.title} description={item.description} src={require("./images/" + item.src[0])} id={item.id} tools={item.tools} type={item.type} key={item.id} demo={item.demoLink} code={item.codeLink}/>)}
     </div>
 
     if(projectToShow !== -1){
